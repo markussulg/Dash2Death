3f9bc7386f4c2416233895d1ba304d76f2d37420
Rasmus Talioja <rasmus.talioja@gmail.com>
2023-02-05 15:02:29 +0200

    test

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -2,23 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour {
 
     [SerializeField] private Button serverButton;
-    [SerializeField] private Button hostButton;
-    [SerializeField] private Button clientButton;
 
     private void Awake() {
         serverButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartServer();
-        });
-        hostButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartHost();
-        });
-        clientButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartClient();
+            SceneManager.LoadScene("Menu");
         });
     }
 }
